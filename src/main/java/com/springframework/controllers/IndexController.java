ee24e05ea1b72d4c2999b54310725973e8269162
ragger.break <ragger.break@gmail.com>
2020-07-19 12:51:38 +0200

    9,7: Gotchas with Project Lombok

--- src/main/java/com/springframework/controllers/IndexController.java
+++ src/main/java/com/springframework/controllers/IndexController.java
@@ -1,17 +1,13 @@
 package com.springframework.controllers;
 
-import com.springframework.domain.Category;
-import com.springframework.domain.UnitOfMeasure;
-import com.springframework.repositories.CategoryRepository;
-import com.springframework.repositories.UnitOfMeasureRepository;
 import com.springframework.services.RecipeService;
-import org.hibernate.tool.schema.internal.exec.ScriptTargetOutputToFile;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -23,7 +19,7 @@ public class IndexController {
 
     @RequestMapping({"","/","/index"})
     public String getIndexPage(Model model){
-
+        log.debug("Getting Index page");
         model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
