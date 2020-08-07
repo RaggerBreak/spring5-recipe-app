41662a78803b91b6c74246f0666bec30cdecc182
ragger.break <ragger.break@gmail.com>
2020-08-07 11:59:34 +0200

    11.11: View Ingredients

---
+++ src/test/java/com/springframework/controllers/IngredientControllerTest.java
@@ -0,0 +1,50 @@
+package com.springframework.controllers;
+
+import com.springframework.commands.RecipeCommand;
+import com.springframework.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+class IngredientControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    IngredientController controller;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        controller = new IngredientController(recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    void listIngredients() throws Exception {
+        //given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+        //when
+        mockMvc.perform(get("/recipe/1/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/list"));
+
+        //then
+        verify(recipeService, times(1)).findCommandById(anyLong());
+    }
+}
